4e5e16a6cd212426f7971ae81d1a75c4704ca4e3
Stephan Exius <sexius@westinghousehs.org>
2017-01-22 22:21:29 +0000

    I changed stuff

---
+++ Pages/script.js
@@ -0,0 +1,21 @@
+$(doucment).ready(function(){
+    
+    $("#active").hover(function() {
+    window.location = "/Pages/Pages/index.html";
+});
+    $("#active2").hover(function() {
+    window.location = "/Pages/Pages/events.html";
+});
+    $("#active3").hover(function() {
+    window.location = "/Pages/Pages/news.html";
+});
+    $("#active4").hover(function() {
+    window.location = "/Pages/Pages/events.html";
+});
+    $("#active5").hover(function() {
+    window.location = "/Pages/Pages/gallery.html";
+});
+
+
+
+});
